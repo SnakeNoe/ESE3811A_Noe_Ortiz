ba3c08eac0a9e283d9844a2bfd62aaf7b5987d86
SnakeNoe <142288805+SnakeNoe@users.noreply.github.com>
2024-02-03 11:10:36 -0600

    Added implementation of random numbers generator
    - Added Tarea4.c with the implementation of random numbers generator with libsodium
    - Added vcpkg installation of library

---
+++ Tarea4/Tarea4.c
@@ -0,0 +1,24 @@
+// Tarea 4 - Noe Ortiz """
+
+#include <stdio.h>
+#include "../libsodium/sodium.h"
+
+int main(void) {
+    uint32_t random = 0;
+    const int upperLimit = 2000;
+
+    if (sodium_init() < 0) {
+        printf("Initialization error!\n");
+    }
+
+    random = randombytes_random();
+    printf("Random: %d\n", random);
+    random = randombytes_uniform(upperLimit);
+    printf("Random number with upper limit: %d\n", random);
+
+    if (randombytes_close()) {
+        printf("Erasing error!\n");
+    }
+
+    return 0;
+}
