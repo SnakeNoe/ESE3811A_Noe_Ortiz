25ce80433b303cff107b1e5605fcb6a88483247c
SnakeNoe <142288805+SnakeNoe@users.noreply.github.com>
2024-03-02 13:08:11 -0600

    Fixed minor bug in AES encryption v0.2

--- Proyecto_ESE3811A/gpio_led_output/source/Proyecto.c
+++ Proyecto_ESE3811A/gpio_led_output/source/Proyecto.c
@@ -33,12 +33,13 @@
  */
 int main(void)
 {
-	uint8_t plainText[] = {"Tarea 07 - Noe Ortiz"};
+	uint8_t plainText[111] = {"Este es el proyecto final de la materia ingenieria de software y seguridad para sistemas embebidos - Noe Ortiz"};
 	uint8_t key[] = {0x03, 0x68, 0x22, 0xA9, 0x04, 0xAA, 0x41, 0x6E, 0x11, 0xFE, 0x3E, 0x5B, 0xC9, 0x8C, 0xA1, 0x33};
-	uint8_t initVector[]  = {0x03, 0x68, 0x22, 0xA9, 0x04, 0xAA, 0x41, 0x6E, 0x11, 0xFE, 0x3E, 0x5B, 0xC9, 0x8C, 0xA1, 0x33};
+	uint8_t initVector[]  = {0x76, 0x25, 0x11, 0x4E, 0x1E, 0xEE, 0xAA, 0x9B, 0x5A, 0x31, 0x5C, 0x15, 0xC7, 0x90, 0xAD, 0x03};
 	struct AES_ctx ctx;
 	size_t plainTextLen, buffLen;
 	uint8_t buff[512] = {0};
+	uint8_t encryptedLen = 0;
 
     /* Board pin, clock, debug console init */
     BOARD_InitBootPins();
@@ -46,9 +47,9 @@ int main(void)
     BOARD_InitDebugConsole();
 
     PRINTF("AES encryption\r\n");
-    PRINTF("----------------------------------------------------------------------------------------\r\n");
-    AES_init_ctx_iv(&ctx, key, initVector);
+    PRINTF("----------------------------------------------------------------------------------------\r");
 
+    AES_init_ctx_iv(&ctx, key, initVector);
     plainTextLen = strlen(plainText);
     buffLen = plainTextLen + (16 - (plainTextLen % 16) );
 	memcpy(buff, plainText, plainTextLen);
@@ -57,16 +58,12 @@ int main(void)
 	PRINTF("Encrypted data: ");
 	for(int i=0; i<buffLen; i++) {
 		PRINTF("0x%02x ", buff[i]);
+		encryptedLen += 1;
 	}
 	PRINTF("\r\n");
-	PRINTF("----------------------------------------------------------------------------------------\r\n");
+	PRINTF("Length: %d\r\n", encryptedLen);
 
-	PRINTF("Encrypted raw data: ");
-	for(int i=0; i<buffLen; i++) {
-		PRINTF("%c ", buff[i]);
-	}
-	PRINTF("\r\n\n");
-	PRINTF("----------------------------------------------------------------------------------------\r\n");
+	PRINTF("----------------------------------------------------------------------------------------\r");
 
 	AES_init_ctx_iv(&ctx, key, initVector);
 
@@ -77,6 +74,7 @@ int main(void)
 		PRINTF("%c", buff[i]);
 	}
 	PRINTF("\r\n");
+	PRINTF("Length: %d\r\n\n", strlen(buff));
 
     while (1);
 }
